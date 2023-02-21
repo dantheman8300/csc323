5ccb4fb449da52a2f5a2c3428587096cce5177ab
eliseemelia <eliseemelia@gmail.com>
2023-02-21 11:36:55 -0800

    added cookies

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,6 +1,6 @@
 from Crypto.Cipher import AES
 import base64
-
+import requests
 
 ## TASK I: PADDING FOR BLOCK CIPHERS
 # implement pkcs#7
@@ -37,7 +37,6 @@ def unpad(txt):
     return bytes(barr)
 
 
-
 ## TASK II: ECB MODE
 
 # implement ecb mode
@@ -58,11 +57,6 @@ def ebc_encrypt(key, pt, out):
 
 
 
-file_e = open("lab2\Lab2_TaskII_A.txt", 'rb')
-f_out = open("lab2\out.txt", "wb")
-# ebc_encrypt(b'1234567812345678', file_e, f_out)
-file_e.close()
-f_out.close()
 
 # take in a ciphertext and a key, 
 # decrypt the ciphertext, 
@@ -83,20 +77,6 @@ def ecb_decrypt(cipher, key, out):
 
 
 
-file_d = open("lab2\Lab2_TaskII_A.txt", 'r')
-
-cipher = open("lab2\decode.txt", "wb")
-cipher.write(base64.b64decode(file_d.read()))
-cipher.close()
-
-ct = open("lab2\decode.txt", "rb")
-f_out2 = open("lab2\out2.txt", "wb")
-ecb_decrypt(ct, b'CALIFORNIA LOVE!', f_out2)
-ct.close()
-file_d.close()
-f_out2.close()
-
-
 ## TASK II : identify ECB mode
 # 100 hex encoded bmp files
 def detect_ecb():
@@ -110,9 +90,81 @@ def read_BMPs(file):
         hex_decode.write(bytes.fromhex(line))
         hex_decode.close()
 
+# def score_BMPs():
 
-file_b = open("lab2\Lab2.TaskII.B.txt", "r")
-read_BMPs(file_b.readlines())
+
+def task2b():
+    file_b = open("lab2\Lab2.TaskII.B.txt", "r")
+    # read_BMPs(file_b.readlines())
 
 
 ## TASK II:  ECB Cookies
+# create a valid cookie that gives admin access
+# user=USERNAME&uid=UID&role=ROLE
+
+def ECB_Cookies():
+    session = requests.Session()
+    url = 'http://localhost:8080/register'
+    url2 = 'http://localhost:8080/'
+    url3 = 'http://localhost:8080/home'
+    username = "aaaaaaaaaaauser\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0Aadmin\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0Buser\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0A"
+    password = 'a'
+
+    # register
+    z = session.post(url, {"user" : username, "password" : password})
+
+    # login
+    y = session.post(url2, {"user" : username, "password" : password})
+
+    # get cookie on home page
+    x = session.get(url3)
+    cookie = session.cookies['auth_token']
+
+    print(cookie)
+
+
+
+ECB_Cookies()
+# user=USERNAME&uid=UID&role=ROLE
+# *******role=
+# admin*********** (11 left) 
+
+
+# user=aaaaaaaaaaa
+# user00000000000A
+# admin0000000000B
+# user************
+# **********
+# aaauser*aaaaaaaa
+# uid=
+# ***********role=
+# admin***********
+
+
+
+
+
+
+
+
+def task2_encrypt():
+    file_e = open("lab2\Lab2_TaskII_A.txt", 'rb')
+    f_out = open("lab2\out.txt", "wb")
+    # ebc_encrypt(b'1234567812345678', file_e, f_out)
+    file_e.close()
+    f_out.close()
+
+
+def task2a():
+    file_d = open("lab2\Lab2_TaskII_A.txt", 'r')
+
+    cipher = open("lab2\decode.txt", "wb")
+    cipher.write(base64.b64decode(file_d.read()))
+    cipher.close()
+
+    ct = open("lab2\decode.txt", "rb")
+    f_out2 = open("lab2\out2.txt", "wb")
+    ecb_decrypt(ct, b'CALIFORNIA LOVE!', f_out2)
+    ct.close()
+    file_d.close()
+    f_out2.close()
