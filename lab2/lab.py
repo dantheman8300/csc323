8ca45d4c663ad9d6268ecf0a9475777861449851
Daniel-Astriddao <daniel@astriddao.xyz>
2023-02-23 10:00:37 -0800

    progress

--- lab2/lab.py
+++ lab2/lab.py
@@ -4,14 +4,14 @@ from Crypto.Cipher import AES
 
 import requests
 
-def encrypt_ecb(fileIn: str, fileOut: str, key=b'1234567812345678', bmp = False):
+def encrypt_ecb(fileNameIn: str, fileNameOut: str, key=b'1234567812345678', bmp = False):
   # Read input file
-  fileIn = open(fileIn, "rb")
+  fileIn = open(fileNameIn, "rb")
   if (bmp):
     headerBytes = fileIn.read(54) # store header info
 
   # Create output file
-  fileOut = open(fileOut, 'wb')
+  fileOut = open(fileNameOut, 'wb')
   if (bmp):
     fileOut.write(headerBytes)
 
@@ -39,11 +39,11 @@ def encrypt_ecb(fileIn: str, fileOut: str, key=b'1234567812345678', bmp = False)
     blockCipherText = cipher.encrypt(block)
     fileOut.write(blockCipherText)
 
-def decrypt_ecb(fileIn: str, fileOut: str, key=b'1234567812345678', bmp = False):
+def decrypt_ecb(fileNameIn: str, fileNameOut: str, key=b'1234567812345678', bmp = False):
   # Read input file
-  fileIn = open(fileIn, "rb")
+  fileIn = open(fileNameIn, "rb")
   # Create output file
-  fileOut = open(fileOut, 'wb')
+  fileOut = open(fileNameOut, 'wb')
 
   if (bmp):
     headerBytes = fileIn.read(54) # store header info
@@ -69,29 +69,31 @@ def decrypt_ecb(fileIn: str, fileOut: str, key=b'1234567812345678', bmp = False)
     else:
       fileOut.write(blockCipherText)
 
-def encrypt_cbc() -> bytes:
+def encrypt_cbc(fileNameIn: str, fileNameOut: str, key=b'1234567812345678',  bmp = False, iv = randbytes(16)) -> bytes:
   # Read input file
-  fileIn = open("mustang.bmp", "rb")
-  headerBytes = fileIn.read(54) # store header info
+  fileIn = open(fileNameIn, "rb")
+  if (bmp):
+    headerBytes = fileIn.read(54) # store header info
 
   # Create output file
-  fileOut = open("mustang-e-cbc.bmp", 'wb')
-  fileOut.write(headerBytes)
+  fileOut = open(fileNameOut, 'wb')
+  if (bmp):
+    fileOut.write(headerBytes)
 
   # Read first block of data
   block = fileIn.read(16)
+  padded = False
 
   # gernerate IV
-  prevCipherText = randbytes(16)
-  iv = prevCipherText
+  prevCipherText = iv
 
   while(len(block) > 0):
     # create aes cipher
-    key = b'1234567812345678'
     cipher = AES.new(key, AES.MODE_ECB)
 
     if(len(block) < 16):
       block = padd(block)
+      padded = True
 
     blockCipherText = cipher.encrypt(xor(block, prevCipherText))
     fileOut.write(blockCipherText)
@@ -99,33 +101,166 @@ def encrypt_cbc() -> bytes:
     prevCipherText = blockCipherText
     block = fileIn.read(16)
 
+  # add full padded block if needed
+  if (not padded):
+    block = padd(b'')
+    cipher = AES.new(key, AES.MODE_ECB)
+    blockCipherText = cipher.encrypt(xor(block, prevCipherText))
+    fileOut.write(blockCipherText)
+
   return iv
 
-# user=USERNAaaaaa
+def decrypt_cbc(fileNameIn: str, fileNameOut: str, iv, key, bmp = False) -> bytes:
+  # Read input file
+  fileIn = open(fileNameIn, "rb")
+  if (bmp):
+    headerBytes = fileIn.read(54) # store header info
+
+  # Create output file
+  fileOut = open(fileNameOut, 'wb')
+  if (bmp):
+    fileOut.write(headerBytes)
+
+  # Read first block of data
+  block = fileIn.read(16)
+
+  # gernerate IV
+  prevCipherText = iv
+
+  while(len(block) > 0):
+    # create aes cipher
+    cipher = AES.new(key, AES.MODE_ECB)
+
+    blockCipherText = cipher.decrypt(xor(block, prevCipherText))
+    fileOut.write(blockCipherText)
+
+    prevCipherText = blockCipherText
+    block = fileIn.read(16)
+
+    # if (len(block) == 0):
+    #   blockCipherText = unpadd(blockCipherText)
+    #   fileOut.write(blockCipherText)
+    # else:
+    #   fileOut.write(blockCipherText)
+
+  return iv
+
+def decrypt_cbc_actual(fileNameIn: str, fileNameOut: str, iv: bytes, key, bmp = False):
+  # Read input file
+  fileIn = open(fileNameIn, "rb")
+  if (bmp):
+    headerBytes = fileIn.read(54) # store header info
+
+  # Create output file
+  fileOut = open(fileNameOut, 'wb')
+  if (bmp):
+    fileOut.write(headerBytes)
+
+  # Read first block of data
+  block = fileIn.read()
+
+  # Decrypt
+
+  # create aes cipher
+
+  cipher = AES.new(key, AES.MODE_CBC, iv)
+
+  blockCipherText = cipher.decrypt(block)
+  fileOut.write(blockCipherText)
+
+# user=12345678912
 # user------------
-# admin***********
-# user************
-# **&uid=UID&role=
+# admin-----------
+# user------------
+# 123&uid=12&role=
 # user------------
 
-registerURL = 'http://localhost:8080/register'
-loginURL = 'http://localhost:8080/'
-homeURL = 'http://localhost:8080/home'
-x = requests.post(registerURL, {"user": "USERNAaaaaa"+"user\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0A" + "", "password": "f"})
-print(x.text)
+def ECBCookies():
+  session = requests.Session()
+  url = 'http://localhost:8080/register'
+  url2 = 'http://localhost:8080/'
+  url3 = 'http://localhost:8080/home'
+  username = "12345678912" + "user\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0C" + "admin\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0B" + "user\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0C" + "123"
+  password = 'a'
 
-x = requests.post(loginURL, {"user": "USERNAaaaaa"+"user\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0A" + "", "password": "f"})
-print(x.text)
-print(x.cookies)
+  # register
+  z = session.post(url, {"user" : username, "password" : password})
 
-# initialize a session
-session = requests.Session()
-  
-# send a get request to the server
-response = session.get(loginURL)
-  
-# print the response dictionary
-print(session.cookies.get_dict())
+  # login
+  y = session.post(url2, {"user" : username, "password" : password})
+
+  # get cookie on home page
+  x = session.get(url3)
+  cookie = session.cookies['auth_token']
+
+  # print cookie in blocks of 16 bytes
+  print("OG Cookie: ")
+  for i in range(0, len(cookie), 32):
+    print(cookie[i:i+32])
+
+  # attack cookie - replace the last 32 characters with the third 32 character block
+  cookie = cookie[:160] + cookie[64:96]
+
+  # print cookie in blocks of 16 bytes
+  print("Attacked Cookie: ")
+  for i in range(0, len(cookie), 32):
+    print(cookie[i:i+32])
+
+  print("cookie: {}".format(cookie))
+
+# 1234567812345678
+
+# user=123456789&u
+# id=2&role=user--
+
+def CBCCookies():
+  session = requests.Session()
+  url = 'http://localhost:8080/register'
+  url2 = 'http://localhost:8080/'
+  url3 = 'http://localhost:8080/home'
+  username = '123456789'
+  password = 'a'
+
+  # register
+  z = session.post(url, {"user" : username, "password" : password})
+
+  # login
+  y = session.post(url2, {"user" : username, "password" : password})
+
+  # get cookie on home page
+  x = session.get(url3)
+  cookie = session.cookies['auth_token']
+
+  # print cookie in blocks of 16 bytes
+  print("OG Cookie: ")
+  for i in range(0, len(cookie), 32):
+    print(cookie[i:i+32])
+
+  # attack cookie - perform bit flipping attack
+  cookie = bytearray.fromhex(cookie)
+  cookie[10] = cookie[10] ^ ord('u') ^ ord('a')
+  cookie[11] = cookie[11] ^ ord('s') ^ ord('d')
+  cookie[12] = cookie[12] ^ ord('e') ^ ord('m')
+  cookie[13] = cookie[13] ^ ord('r') ^ ord('i')
+  cookie[14] = cookie[14] ^ ord('\x00') ^ ord('n')
+  cookie[15] = cookie[15] ^ ord('\x02') ^ ord('\x01')
+
+  cookie = cookie.hex()
+
+  # print attacked cookie in blocks of 16 bytes
+  print("Attacked Cookie: ")
+  for i in range(0, len(cookie), 32):
+    print(cookie[i:i+32])
+
+  print('cookie: {}'.format(cookie))
+
+
+  # # print cookie in blocks of 16 bytes
+  # print("Attacked Cookie: ")
+  # for i in range(0, len(cookie), 32):
+  #   print(cookie[i:i+32])
+
+  # print("cookie: {}".format(cookie))
 
 # Take a file with hex encoded BMPs, seperated by a new line, and decode them
 # into a file with the decoded BMPs
@@ -175,6 +310,16 @@ def base64DecodeFile(fileIn: str, fileOut: str):
 
   fileOut.write(base64.b64decode(fileIn.read()))
 
+def xor(a: bytes, b: bytes) -> bytes:
+  xorBytes = b''
+
+  for i in range(len(a)):
+    xorBit = a[i]^b[i]
+    xorBitByte = xorBit.to_bytes(1, 'big')
+    xorBytes += xorBitByte
+
+  return xorBytes  
+
 # block = b''
 # for _ in range(16):
 #   print(block)
@@ -187,3 +332,14 @@ def base64DecodeFile(fileIn: str, fileOut: str):
 # decrypt_ecb("Lab2.TaskII.A.decoded.txt", "Lab2.TaskII.A.decrypted.txt", key = b'CALIFORNIA LOVE!')
 
 # parseBMPsFromFile("Lab2.TaskII.B.txt", "decodedBMP")
+
+# ECBCookies()  
+
+CBCCookies()
+
+# base64DecodeFile("Lab2.TaskIII.A.txt", "Lab2.TaskIII.A.decoded.txt")
+# decrypt_cbc_actual("Lab2.TaskIII.A.decoded.txt", "Lab2.TaskIII.A.decrypted.actual.txt", key = b'MIND ON MY MONEY', iv=b'MONEY ON MY MIND')
+# decrypt_cbc("Lab2.TaskIII.A.decoded.txt", "Lab2.TaskIII.A.decrypted.txt", key = b'MIND ON MY MONEY', iv=b'MONEY ON MY MIND')
+# iv = encrypt_cbc("cp-logo.bmp", "cp-logo-cbc.bmp", key = b'CALIFORNIA LOVE!', bmp=True)
+# decrypt_cbc("cp-logo-cbc.bmp", "cp-logo-cbc-decrypted.bmp", iv, key = b'CALIFORNIA LOVE!', bmp=True)
+# decrypt_cbc_actual("cp-logo-cbc.bmp", "cp-logo-cbc-decrypted.bmp", iv)
