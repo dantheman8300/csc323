5302d68d21ad1d00be14ee068bcec5cf33ddb4aa
eliseemelia <eliseemelia@gmail.com>
2023-03-02 10:30:51 -0800

    updated lab3

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -4,14 +4,28 @@ def pad_oracle():
 
     url= 'http://localhost:8080/'
     url2= 'http://localhost:8080/eavesdrop'
+    url3= 'http://localhost:8080/submit'
 
+    guess = '9'
 
-    req = requests.get(url2)
+# open ssl - speed test (how many hashes you can do per sec)
+    req = requests.get(url2).text
     check = requests.get(url)
 
+    cipher = req.split("<font color=\"red\"> ")[1].split(" </font></p>")[0]
+    print("Leaked cipher text:" + cipher)
 
-    print(req)
-    print(check)
+
+    
+
+
+    # print(check)
+
+
+
+    
+    # submit = requests.post(url3, {"guess" : guess}).text.split("<font color=\"red\"> ")[1].split(" </font></p>")[0]
+    # print("result: " + submit)
 
 
 pad_oracle()
