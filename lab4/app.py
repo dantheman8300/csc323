c8dcec38c087658f696ce6af3dafad33e42583b1
dantheman8300 <daniel.7039@hotmail.com>
2023-03-13 18:12:01 -0700

    finished app v1

--- lab4/app.py
+++ lab4/app.py
@@ -168,5 +168,27 @@ blocks = [
 def index():
     return render_template('index.html', blocks=blocks)
 
+@app.route('/blocks')
+def view_blocks():
+    return render_template('blocks.html', blocks=blocks)
+
+@app.route('/accounts')
+def public_keys():
+    # Create a dictionary to store the balances for each public key
+    balances = {}
+
+    # Loop through each block in the blockchain and update the balances for each public key
+    for block in blocks:
+        for output in block['tx']['output']:
+            pub_key = output['pub_key']
+            value = output['value']
+            if pub_key in balances:
+                balances[pub_key] += value
+            else:
+                balances[pub_key] = value
+
+    # Render the template with the balances
+    return render_template("accounts.html", public_keys=balances)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="localhost", port=5001, debug=True)
